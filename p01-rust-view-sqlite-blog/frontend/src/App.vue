9ff4dff87b59db7ee1b8689847f6bcb6fda9bcad
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-23 18:27:08 -0500

    Update plan notes

--- p01-rust-view-sqlite-blog/frontend/src/App.vue
+++ p01-rust-view-sqlite-blog/frontend/src/App.vue
@@ -1,8 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  Hello World, this is Vue App
+  <div class="mx-auto flex justify-center h-screen items-center">
+    <h1 class="text-2xl">Hello World, this is Vue App</h1>
+  </div>
 </template>
-
-
