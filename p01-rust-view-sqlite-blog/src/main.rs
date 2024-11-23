3b8cbc9187ecd244c29bd2bcceee1bc154ad1984
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-23 17:57:57 -0500

    Add starting point of Vue front end

--- p01-rust-view-sqlite-blog/src/main.rs
+++ p01-rust-view-sqlite-blog/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+#[macro_use]
+extern crate rocket;
 
+use rocket::fs::{relative, FileServer};
+
+// #[get("/")]
+// fn index() -> &'static str {
+//     "hello world"
+// }
+
+#[launch]
+fn rocket() -> _ {
+    // rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", FileServer::from(relative!("frontend/dist")))
 }
