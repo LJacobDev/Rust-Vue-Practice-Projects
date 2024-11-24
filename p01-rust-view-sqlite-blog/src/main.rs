1015b60ad584192537ab3747be14c25d446ab99c
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-23 19:32:41 -0500

    Clean up commented out lines

--- p01-rust-view-sqlite-blog/src/main.rs
+++ p01-rust-view-sqlite-blog/src/main.rs
@@ -3,13 +3,7 @@ extern crate rocket;
 
 use rocket::fs::{relative, FileServer};
 
-// #[get("/")]
-// fn index() -> &'static str {
-//     "hello world"
-// }
-
 #[launch]
 fn rocket() -> _ {
-    // rocket::build().mount("/", routes![index])
     rocket::build().mount("/", FileServer::from(relative!("frontend/dist")))
 }
